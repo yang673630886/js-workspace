7bfc2ee4b3eeb432f24f48cbad61367c53074046
yangjingsheng <673630886@qq.com>
2019-08-21 20:25:18 +0800

    提交作业

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,17 @@
-const add = require('../main');
+const testDemo = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+it ('should return multiply table from 2 to 4 when call testDemo', () => {
+    expect(testDemo(2, 4)).toBe("2*2=4\n2*3=6 3*3=9\n2*4=8 3*4=12 4*4=16");
+});
+
+it ('should return start must less than end when call testDemo', () =>{
+    expect(testDemo(4,2)).toBe("左边应小于右边");
+});
+
+it ('should return start must less than end when call testDemo', () =>{
+    expect(testDemo(1,9999)).toBe("数字不是1到1000之间");
+});
+
+it ('should return start must less than end when call testDemo', () =>{
+    expect(testDemo(1,1.1)).toBe("输入不是整数");
 });
--- main.js
+++ main.js
@@ -1,5 +1,57 @@
-function add (left, right) {
-    return left + right;
+function isVaildNum(start, end) {
+    var startIsInteger = Number.isInteger(start);
+    var endIsInteger = Number.isInteger(end);
+    if (startIsInteger == true, endIsInteger == true) {
+        return true;
+    }
+    else {
+        return "输入不是整数";
+    }
 }
 
-module.exports = add;
+function isStartLessEnd(start, end) {
+    if (start <= end) {
+        return true;
+    }
+    else {
+        return "左边应小于右边";
+    }
+}
+
+function is1To1000(start, end) {
+    if (start >= 1, start <= 1000, end >= 1, end <= 1000) {
+        return true;
+    }
+    else return "数字不是1到1000之间";
+}
+
+function createMultiplyTable(start, end) {
+    let multiplyTable = [];
+    for (let i = start; i <= end; i++) {
+        for (let j = start; j <= i; j++) {
+            multiplyTable += j + "*" + i + "=" + i * j;
+            if (i !== j) {
+                multiplyTable += " "
+            }
+        }
+        if (i < end) {
+            multiplyTable += "\n";
+        }
+    }
+    return multiplyTable;
+}
+
+function testDemo(start, end) {
+    if (isStartLessEnd(start, end) == true) {
+        if (isVaildNum(start, end) == true) {
+            if (is1To1000(start, end) == true) {
+                return createMultiplyTable(start, end);
+            }
+            else return is1To1000(start, end);
+        }
+        else return isVaildNum(start, end);
+    }
+    else return isStartLessEnd(start, end);
+}
+
+module.exports = testDemo;
